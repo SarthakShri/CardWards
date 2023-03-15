7b7dda35765df1ce0901d502a4994965c5c96ecb
Sarthak Shrivastava <sarthakrishu@gmail.com>
2023-03-16 01:43:47 +0530

    Initial Commit

---
+++ Card Wars/Card_WarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_WarsApp.swift
+//  Card Wars
+//
+//  Created by Sarthak Shrivastava on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Card_WarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
